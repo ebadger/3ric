8d5ff1791272fe567a706edbe54aacb181bbfd18
Eric Badger <ebadger@microsoft.com>
2024-04-27 08:55:15 -0700

    fix some shutdown crashing bugs

--- emulator/Badger6502Emulator/MainWindow.xaml.cpp
+++ emulator/Badger6502Emulator/MainWindow.xaml.cpp
@@ -440,7 +440,7 @@ namespace winrt::Badger6502Emulator::implementation
 
     void MainWindow::Cleanup()
     {
-
+        _timer.Stop();
         UpdateExecutionState(ExecutionState::Quit);
         WaitForSingleObject(_hThread, INFINITE);
 
@@ -2546,7 +2546,10 @@ namespace winrt::Badger6502Emulator::implementation
 
     void MainWindow::ProcessHires(int page)
     {
-        EnterCriticalSection(&_cs);
+        if (false == TryEnterCriticalSection(&_cs))
+        {
+            return;
+        }
 
         for (auto pItem : _mapHires[page])
         {
@@ -2586,7 +2589,11 @@ namespace winrt::Badger6502Emulator::implementation
         }
         */
 
-        EnterCriticalSection(&_cs);
+        if (false == TryEnterCriticalSection(&_cs))
+        {
+            return;
+        }
+
         for (auto pItem : _mapText[page])
         {
             _text[page][pItem.second.address] = pItem.second.data;
